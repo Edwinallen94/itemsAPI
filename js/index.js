7dc6dadc89e4a4f81afeec3c3cd83b023e2d22f1
Maliha <khanmaliha726@gmail.com>
2022-11-06 01:34:29 -0600

    fixed card creation

--- js/index.js
+++ js/index.js
@@ -3,13 +3,14 @@ const productsController = new ProductsController(0);
 
 
 function addItemCard(item) {
-  const itemHTML =
-  `<div class="card h-100 product-card">
+  let itemHTML =
+  `<div class="card h-100 product-card>
   <img
-    src= ${item.imageUrl}
+    src= "${item.imageUrl}"
     class="card-img-top"
+    id="cimg"
   />
-  <div class="card-body">
+  <div class="card-body" id="cbod">
     <h5 class="card-title">${item.name}</h5>
     <p class="card-text">${item.description}</p>
   </div>
@@ -19,8 +20,9 @@ function addItemCard(item) {
   </div>
 </div>`;
 
-  const itemsContainer = document.getElementById("list-items");
+  let itemsContainer = document.getElementById("list-items");
   itemsContainer.innerHTML += itemHTML;
+
 }
 
 
@@ -29,70 +31,31 @@ function loadStorageSampleData(){
   if(!localStorage.getItem("products"))
   {
     const productItems =[{'name':'Boho Style',
-    'imgURL':'/pictures/boho-bedding-urban-bedding.jpg',
-    'description':'Vibrant boho bedding'},{'name':'Canopy Style',
-    'imgURL':'/pictures/canopy-bed.jpg',
-    'description':'Elegant canopy bedding'},{'name':'Farmhouse Style',
-    'imgURL':'/pictures/farmhouse-bed-frame.jpg',
-    'description':'Wood framhouse bedding'}];
+    'imageUrl':'../pictures/boho-bedding-urban-bedding.jpg',
+    'description':'Vibrant boho bedding','price':'Price'},{'name':'Canopy Style',
+    'imageUrl':'https://www.decorsnob.com/wp-content/uploads/cutest-canopy-bed-I-hung-my-plants-on-it.jpg',
+    'description':'Elegant canopy bedding','price':'Price'},{'name':'Farmhouse Style',
+    'imageUrl':'https://i.pinimg.com/564x/8b/d1/82/8bd182012328e66c09f5c7c0db22816b--farmhouse-style-headboard-diy-farmhouse-bed-frame-diy.jpg',
+    'description':'Wood framhouse bedding','price':'Price'}];
 
     localStorage.setItem("products", JSON.stringify(productItems));
   }
 }
 
 
+
 function loadCardsListFromProductController(){
-  for(let i = 0, size = productsController.products.length; i < size ; i++){
-      const item = productsController.products[i];
+  for(let i = 0; i < productsController.products.length; i++){
+      let item = productsController.products[i];
       addItemCard(item);
       
   }
 
 }
 
-productsController.loadItemsFromLocalStorage;
 loadStorageSampleData();
+productsController.loadItemsFromLocalStorage();
 loadCardsListFromProductController();
-
-
-
-
-
-/*
-function createDoms(){
-  for(let i = 0; i < productsController.products.length; i++ )
-  {
-      const addcard = document.createElement("div");
-      addcard.setAttribute('class','card h-100 product-card');
-      const addimg = document.createElement("img");
-      addimg.setAttribute('src', json.ProductsController.products[i].imageURL);
-      addimg.setAttribute('class','card-img-top');
-      addcard.appendChild(addimg);
-      const addcardbody = document.createElement('div');
-      addcardbody.setAttribute('class','card-body');
-      const addheadingfive = document.createElement('h5');
-      addheadingfive.setAttribute('class','card-title');
-      const addp = document.createElement('p');
-      addp.setAttribute('class','card-text');
-      addheadingfive.innerHTML = json.ProductsController.products[i].name;
-      addp.innerHTML = json.ProductsController.products[i].description;
-      addcard.appendChild(addcardbody);
-      addcardbody.appendChild(addheadingfive);
-      addcardbody.appendChild(addp);
-      const addcardfooter = document.createElement("div");
-      addcardfooter.setAttribute('class','card-footer');
-      const addsmalltag = document.createElement("small");
-      addsmalltag.setAttribute('class','text-muted');
-      addsmalltag.innerHTML = json.ProductsController.products[i].price;
-      addcard.appendChild(addcardfooter);
-      addcardfooter.appendChild(addsmalltag);
-
-
-
-
-  }
-}
-*/
 
 
 
