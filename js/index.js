519d1164643d62146811ce27e14504253cee5320
ShanteTorres <kennedy2110@gmail.com>
2022-11-07 13:08:54 -0500

    added item-form.js

--- js/index.js
+++ js/index.js
@@ -8,14 +8,14 @@ function addItemCard(item) {
   <img
     src= "${item.imageUrl}"
     class="card-img-top"
-    id="cimg"
+    id="new-item-img"
   />
   <div class="card-body" id="cbod">
-    <h5 class="card-title">${item.name}</h5>
-    <p class="card-text">${item.description}</p>
+    <h5 class="card-title" id="new-item-name">${item.name}</h5>
+    <p class="card-text" id="new-item-description">${item.description}</p>
   </div>
   <div class="card-footer">
-    <small class="text-muted">${item.price}</small>
+    <small class="text-muted" id="new-item-price">${item.price}</small>
     <div id="list-items></div>
   </div>
 </div>`;
@@ -26,21 +26,21 @@ function addItemCard(item) {
 }
 
 
-function loadStorageSampleData(){
+// function loadStorageSampleData(){
 
-  if(!localStorage.getItem("products"))
-  {
-    const productItems =[{'name':'Boho Style',
-    'imageUrl':'../pictures/boho-bedding-urban-bedding.jpg',
-    'description':'Vibrant boho bedding','price':'Price'},{'name':'Canopy Style',
-    'imageUrl':'https://www.decorsnob.com/wp-content/uploads/cutest-canopy-bed-I-hung-my-plants-on-it.jpg',
-    'description':'Elegant canopy bedding','price':'Price'},{'name':'Farmhouse Style',
-    'imageUrl':'https://i.pinimg.com/564x/8b/d1/82/8bd182012328e66c09f5c7c0db22816b--farmhouse-style-headboard-diy-farmhouse-bed-frame-diy.jpg',
-    'description':'Wood framhouse bedding','price':'Price'}];
+//   if(!localStorage.getItem("products"))
+//   {
+//     const productItems =[{'name':'Boho Style',
+//     'imageUrl':'../pictures/boho-bedding-urban-bedding.jpg',
+//     'description':'Vibrant boho bedding','price':'Price'},{'name':'Canopy Style',
+//     'imageUrl':'https://www.decorsnob.com/wp-content/uploads/cutest-canopy-bed-I-hung-my-plants-on-it.jpg',
+//     'description':'Elegant canopy bedding','price':'Price'},{'name':'Farmhouse Style',
+//     'imageUrl':'https://i.pinimg.com/564x/8b/d1/82/8bd182012328e66c09f5c7c0db22816b--farmhouse-style-headboard-diy-farmhouse-bed-frame-diy.jpg',
+//     'description':'Wood framhouse bedding','price':'Price'}];
 
-    localStorage.setItem("products", JSON.stringify(productItems));
-  }
-}
+//     localStorage.setItem("products", JSON.stringify(productItems));
+//   }
+// }
 
 
 
@@ -53,7 +53,7 @@ function loadCardsListFromProductController(){
 
 }
 
-loadStorageSampleData();
+// loadStorageSampleData();
 productsController.loadItemsFromLocalStorage();
 loadCardsListFromProductController();
 
--- js/item-form.js
+++ js/item-form.js
@@ -1,1 +1,40 @@
 import { ProductsController } from "./itemsController.js";
+
+
+
+// Initialize a new ItemsController with currentId set to 0
+const newProductsController = new ProductsController(0);
+
+// Select the New Item Form
+const newItemForm = document.getElementById('new-item-form');
+
+// Add an 'onsubmit' event listener
+newItemForm.addEventListener('submit', (event) => {
+    // Prevent default action
+    event.preventDefault();
+
+    // Select the inputs
+    const newItemName = document.getElementById('new-item-name');
+    const newItemDescription = document.getElementById('new-item-description');
+    const newItemImageUrl = document.getElementById('new-item-img');    
+    const newItemPrice = document.getElementById('new-item-price');
+
+    // Get the values of the inputs
+    const name = newItemName.value;
+    const description = newItemDescription.value;
+    const imageUrl = newItemImageUrl.value;
+    const itemPrice = newItemPrice.value;
+
+    /*
+        Validation code here
+    */
+
+    // Add the item to the ItemsController
+    newProductsController.addItem(name, description, imageUrl, itemPrice);
+
+    // Clear the form
+    newItemName.value = '';
+    newItemDescription.value = '';    
+    newItemImageUrl.value = '';
+    newItemPrice.value = '';
+});
